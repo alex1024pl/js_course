64b6f169b2a2801a881cd9f9aad403a93dc51883
alex1024pl <42874246+alex1024pl@users.noreply.github.com>
2018-09-16 19:29:11 +0300

    Add files via upload

---
+++ lesson_8/index.js
@@ -0,0 +1,25 @@
+'use strict'
+const firstname = "Храпач ";
+const name = "Александр";
+console.log(firstname + name);
+
+let a = 13, b = 6;
+
+if (a > b) 
+{
+	console.log (a);
+} if (a < b	) 
+{
+	console.log (b);
+}
+
+if (a > b) 
+{
+	console.log (b);
+} if (a < b	) 
+{
+	console.log (a);
+}
+
+console.log(a+b);
+console.log(a%b);
