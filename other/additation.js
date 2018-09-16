7c143107972e5289c057e356c139628a22120fa1
alex1024pl <42874246+alex1024pl@users.noreply.github.com>
2018-09-16 18:13:53 +0300

    first js

---
+++ other/additation.js
@@ -0,0 +1,10 @@
+'use strict'
+const a = 5;
+const b = 2;
+
+if (a>b) {
+	console.log(a);
+}
+if (a<b) {
+	console.log(b);
+}
---
+++ other/index.js
@@ -0,0 +1,17 @@
+'use strict'
+const a = 2;
+const b = 3;
+const op = 'div'; //add sub mul div
+
+if(op === 'add') {
+	console.log(a+b);
+}
+if(op === 'sub') {
+	console.log(a-b);
+}
+if (op === 'mul') {
+	console.log(a*b)
+}
+if (op === 'div') {
+	console.log(a/b)
+}
