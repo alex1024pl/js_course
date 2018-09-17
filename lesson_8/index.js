0ee7de81367ad9122d8fdda5df1fa2a7048fed06
alex1024pl <42874246+alex1024pl@users.noreply.github.com>
2018-09-17 10:01:56 +0300

    last

--- lesson_8/index.js
+++ lesson_8/index.js
@@ -3,23 +3,34 @@ const firstname = "Храпач ";
 const name = "Александр";
 console.log(firstname + name);
 
-let a = 13, b = 6;
+let a = 14, b = 11;
 
-if (a > b) 
-{
-	console.log (a);
-} if (a < b	) 
-{
-	console.log (b);
-}
+	if (a > b) 
+	{
+		console.log (a);
+	} if (a < b	) 
+	{
+		console.log (b);
+	}
 
-if (a > b) 
-{
-	console.log (b);
-} if (a < b	) 
-{
-	console.log (a);
-}
+	if (a > b) 
+	{
+		console.log (b);
+	} if (a < b	) 
+	{
+		console.log (a);
+	}
 
-console.log(a+b);
-console.log(a%b);
+console.log(a + b);
+console.log(a % b);
+
+	if (a > b + 10) {
+		console.log(a + 10);
+	}
+	if (a < b) {
+		console.log(9);
+	} 
+		else (!a<b) 
+		{ 
+		console.log(b);
+		}
