dee817cdb8017f58b0f08f20f27119a7f25a824e
alex1024pl <42874246+alex1024pl@users.noreply.github.com>
2018-09-24 16:52:27 +0300

    1
    1

---
+++ lesson_11/index.js
@@ -0,0 +1,36 @@
+'use strict'
+const firstname = "Храпач ";
+const name = "Александр";
+console.log(firstname + name);
+
+let a = 14, b = 11;
+
+	if (a > b) 
+	{
+		console.log (a);
+	} if (a < b	) 
+	{
+		console.log (b);
+	}
+
+	if (a > b) 
+	{
+		console.log (b);
+	} if (a < b	) 
+	{
+		console.log (a);
+	}
+
+console.log(a + b);
+console.log(a % b);
+
+	if (a > b + 10) {
+		console.log(a + 10);
+	}
+	if (a < b) {
+		console.log(9);
+	} 
+		else (!a<b) 
+		{ 
+		console.log(b);
+		}
