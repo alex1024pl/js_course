a74c7a57bb5c193267c0f14bd493575b72e654f0
alex1024pl <42874246+alex1024pl@users.noreply.github.com>
2018-10-04 15:00:11 +0300

    Add files via upload

---
+++ lesson_14/index.js
@@ -0,0 +1,49 @@
+'use strict'
+
+const arr = [
+[1, 0, 0, 0, 0, 0]
+[0, 0, 0, 0, 0, 0]
+[0, 0, 0, 0, 0, 0]
+[0, 0, 0, null, 0, 0]
+[0, 0, 0, 0, 0, 0]
+[0, 0, 0, 0, 0, 0]
+]
+
+function calcNulls(row) {
+// вернет кол null в row 
+}
+
+function main(data) {
+// вернет массив с колличеством null в каждом с подмассивов 
+}
+
+console.log(main(arr)); 
+// [0,0,0,1,0,0]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function hello(name, age) {
+// 	console.log('Hello, my name is ' + name + ' my age ' + age);
+// }
+
+// hello('Boris', 54);
+// hello('petya', 14);
+
+
